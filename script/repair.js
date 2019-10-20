2908f308ad98a3b155026e69555a4e97b1ffbaa2
Zhdanovich Katsiaryna <zhdanovichkat@gmail.com>
2019-10-20 11:54:45 +0300

    fix: fixed buttons

--- script/repair.js
+++ script/repair.js
@@ -10,11 +10,3 @@ mobileBtn.addEventListener('click', () => {
     mobileBtn.innerHTML = "Mobile";
   }
 })
-
-
-
-// document.querySelector('frame').style.width = '375px'; mobile
-// document.querySelector('frame').style.margin = '0 auto'; center
-// document.querySelector('frame').style.width = ''; default
-
-
